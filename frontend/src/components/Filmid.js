0da3f35a3c5957c5edd0b63ddc8f8b232001447a
Roberta1400 <116271698+Roberta1400@users.noreply.github.com>
2024-03-19 17:30:24 +0200

    Lisasin filtri võimaluse
    Lisasin filtri võimaluse, et otsida filme vastavalt žanrile, keelele või vanusepiirangule, kuid pean backendis muutusi tegema, et see töötaks õieti. Hetkel ta näitab kõike filma vastavalt ühele valikule, aga ei suuda arusaada kui on pandud mitu filtrit peale.
    
    Filtrite implementeerimist ja abi sain ChatGPT(funktsioonide handleChange ning kuidas õiget api url koostada), filtirte kohta lugesin: https://codesandbox.io/p/sandbox/select-filtering-with-reactjs-ppbu7?file=%2Fsrc%2FFilter.js%3A18%2C8-22%2C18 ja https://medium.com/@masterrajpatel/how-to-use-onchange-event-in-reactjs-2e68ebfca8a6

--- frontend/src/components/Filmid.js
+++ frontend/src/components/Filmid.js
@@ -1,13 +1,40 @@
-
-import './Filmid.css';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Filmid.css';
 
 function Filmid() {
   const [films, setFilms] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState({
+    žanr: '',
+    vanusepiirang: '',
+    keel: ''
+  });
+
+  const handleŽanrChange = (event) => {
+    setSelectedFilters({ ...selectedFilters, žanr: event.target.value });
+  };
+  
+  const handleVanusepiirangChange = (event) => {
+    setSelectedFilters({ ...selectedFilters, vanusepiirang: event.target.value });
+  };
+  
+  const handleKeelChange = (event) => {
+    setSelectedFilters({ ...selectedFilters, keel: event.target.value });
+  };
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/v1/filmid')
+    let apiURL = 'http://localhost:8080/api/v1/filmid';
+
+    const parameters = Object.entries(selectedFilters)
+      .map(([key, value]) => value && `${key}=${value}`)
+      .filter(Boolean)
+      .join('&');
+
+    if (parameters) {
+      apiURL += `?${parameters}`;
+    }
+
+    fetch(apiURL)
       .then(response => response.json())
       .then(data => {
         setFilms(data);
@@ -15,9 +42,30 @@ function Filmid() {
       .catch(error => {
         console.error('Error fetching films:', error);
       });
-  }, []);
+  }, [selectedFilters]);
 
   return (
+  <div className="container">
+    <div className="filter-container">
+      <select value={selectedFilters.žanr} onChange={handleŽanrChange}>
+        <option value="">All</option>
+        <option value="scifi">SciFi</option>
+        <option value="animatsioon">Animatsioon</option>
+        <option value="drama">Draama</option>
+      </select>
+      <select value={selectedFilters.vanusepiirang} onChange={handleVanusepiirangChange}>
+        <option value="">All</option>
+        <option value="14">14</option>
+        <option value="16">Inglise keel</option>
+        <option value="0">Puudub</option>
+      </select>
+      <select value={selectedFilters.keel} onChange={handleKeelChange}>
+        <option value="">All</option>
+        <option value="eesti">Eesti keel</option>
+        <option value="inglise">Inglise keel</option>
+        <option value="vene">Vene keel</option>
+      </select>
+    </div>
     <div className="film-list">
       {films.map(film => (
         <div key={film.id} className="film">
@@ -28,6 +76,7 @@ function Filmid() {
         </div>
       ))}
     </div>
+  </div>
   );
 }
 
